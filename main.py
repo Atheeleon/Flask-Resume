368106d3c0f4b4e27c1aa6cd0aba1e432858b231
Antonio Martins <poeticum.visum@gmail.com>
2023-02-02 10:57:04 -0300

    Dynamic current job date recalc.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+from variables import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", tempo="1 ano e meio")
+    return render_template("index.html", tempo=tempo)
 
 if __name__ == '__main__':
     app.run()
---
+++ variables.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+
+##### Current job, how long #####
+years = divmod(((datetime.now() - datetime(2021, 6, 7, 0, 0, 0)).total_seconds() / 86400), 365)
+months = divmod(years[1], 30)
+tempo = f"Atualmente, {int(years[0])} ano(s) e {int(months[0])} mes(es)."
+
