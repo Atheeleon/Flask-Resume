b6c1b2f7ede5d8ed582882d0cb9088932413de65
Antonio Martins <poeticum.visum@gmail.com>
2023-02-02 11:39:34 -0300

    Minor fixes
    Current job datetime fix
    HTML typos

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from variables import *
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html", tempo=tempo)
+
 
 if __name__ == '__main__':
     app.run()
--- variables.py
+++ variables.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
-
-##### Current job, how long #####
-years = divmod(((datetime.now() - datetime(2021, 6, 7, 0, 0, 0)).total_seconds() / 86400), 365)
+# Current job, how long #
+years = divmod(((datetime.now() - datetime(2021, 8, 24, 0, 0, 0)).total_seconds() / 86400), 365)
 months = divmod(years[1], 30)
 tempo = f"Atualmente, {int(years[0])} ano(s) e {int(months[0])} mes(es)."
-
