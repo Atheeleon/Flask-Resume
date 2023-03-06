bc6959ac09f0839760bbf8d5d86b26fd398f3f7e
Antonio Martins <poeticum.visum@gmail.com>
2023-03-05 23:47:24 -0300

    Correção de variação da variavel 'tempo'

--- variables.py
+++ variables.py
@@ -4,9 +4,9 @@ from datetime import datetime
 years = divmod(((datetime.now() - datetime(2021, 8, 24, 0, 0, 0)).total_seconds() / 86400), 365)
 months = divmod(years[1], 30)
 
-if int(months[0]) = 0:
+if int(months[0]) == 0:
     tempo = f"Atualmente, {int(years[0])} ano"
-elif int(months[0]) = 1:
+elif int(months[0]) == 1:
     tempo = f"Atualmente, {int(years[0])} ano e {int(months[0])} mes."
 else:
-    tempo = f"Atualmente, {int(years[0])} ano e {int(months[0])} mes(es)."
+    tempo = f"Atualmente, {int(years[0])} ano e {int(months[0])} meses."
