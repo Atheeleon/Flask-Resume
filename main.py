ef5b29e24c9448847b783f3b977778647ed1e693
Antonio Martins <poeticum.visum@gmail.com>
2023-06-21 01:26:24 -0300

    Integrado a função de tempo para o main

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
-from variables import *
+import datetime
+
+def tempo():
+    start_date = datetime.datetime(2021, 8, 24)
+    end_date = datetime.datetime.now()
+
+    duration = end_date - start_date
+
+    years = duration.days // 365
+    months = (duration.days % 365) // 30
+
+    return f"{years} ano{'s' if years != 1 else ''} {f'''e {months} {'mês' if months == 1 else 'meses' }''' if months != 0 else ''}"
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", tempo=tempo)
+    return render_template("index.html", tempo=tempo())
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
